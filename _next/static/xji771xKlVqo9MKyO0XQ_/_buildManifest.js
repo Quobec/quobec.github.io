9e6e62339fb9ef58abb2c6467de6b334d8bf2060
Jakub Kępiński <jakubkepinski87@gmail.com>
2023-02-05 21:05:44 +0100

    Site

---
+++ _next/static/xji771xKlVqo9MKyO0XQ_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-8bf774722a1ea08a.js","static/chunks/pages/index-5636e71d4471b369.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

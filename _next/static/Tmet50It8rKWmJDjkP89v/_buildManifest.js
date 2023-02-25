3d990691825e9429b0da2cec31c7772ca0bbcf65
Jakub Kępiński <jakubkepinski87@gmail.com>
2023-02-25 19:34:28 +0100

    Made articlePictures clickable

---
+++ _next/static/Tmet50It8rKWmJDjkP89v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/130-518a8a8ad988b911.js","static/chunks/pages/index-f6285e12c007baa6.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
